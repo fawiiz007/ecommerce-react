977be686e5c9d9e717f839ce2f122bacf57b4b75
fawiiz007 <fawiiz007@gmail.com>
2026-03-12 14:32:30 +0100

    black sheep

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Checkout from "./pages/Checkout";
 import Auth from "./pages/Auth";
 import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
